96250d0f6734151c6451e90bb78e10d0af962cf1
Stas Tsiba <stascyba1@gmail.com>
2023-05-02 16:44:57 +0300

    over

--- src/components/basketBottomButton/BasketBottomButton.jsx
+++ src/components/basketBottomButton/BasketBottomButton.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './basketBottomButton.scss';
+import { useSelector } from 'react-redux';
 
 export const BasketBottomButton = () => {
+  const { basketItem } = useSelector((state) => state.products);
+
+  const clickPay = () => {
+    
+  };
   return (
     <section className="basketBottomButton__wrapper">
       <Link to="/" className="basketBottomButton__back">
         <img src="image/_Path_.svg" alt="arrow" />
         Come back
       </Link>
-      <button className="basketBottomButton__pay">Pay now</button>
+      <button onClick={clickPay} className="basketBottomButton__pay">
+        Pay now
+      </button>
     </section>
   );
 };
